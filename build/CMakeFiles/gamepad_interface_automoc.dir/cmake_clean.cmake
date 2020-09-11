f059de54ec44c6a2151c4c1bb7401df932a4f9be
hari191197 <hari.191197@gmail.com>
2020-09-11 19:27:39 +0800

    removed build

--- build/CMakeFiles/gamepad_interface_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "gamepad_interface_automoc.cpp"
-  "CMakeFiles/gamepad_interface_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/gamepad_interface_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
